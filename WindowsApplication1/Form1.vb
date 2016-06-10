e07e8aa736dbdbe0ae63a2df2be16b2dbac4d1f9
Melonpan666 <melonpan.strangejourney@gmail.com>
2016-06-10 23:09:19 +0900

    ボタン追加

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -4,4 +4,8 @@
         MsgBox("Hello World")
 
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("Good Morning")
+    End Sub
 End Class
